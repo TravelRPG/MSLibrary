c43cef498f364c412ab0230bf5913751e1b99151
mrminsu <82914006+arkarang@users.noreply.github.com>
2021-08-15 01:24:31 +0900

    Create ThrowingConsumer.java

---
+++ mslibrary-misc/src/main/java/kr/msleague/mslibrary/misc/ThrowingConsumer.java
@@ -0,0 +1,17 @@
+package kr.msleague.mslibrary.misc;
+
+import java.util.function.Consumer;
+
+public interface ThrowingConsumer<T> extends Consumer<T> {
+
+    @Override
+    default void accept(T t) {
+        try{
+            acceptThrowing(t);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    void acceptThrowing(T t) throws Exception;
+}
