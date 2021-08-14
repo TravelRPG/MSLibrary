f4866f75deb4f51bfd51d6a4cb8dcddab9e42d1a
mrminsu <82914006+arkarang@users.noreply.github.com>
2021-08-15 05:10:17 +0900

    implemented MySQLDatabase

--- mslibrary-database/src/main/java/kr/msleague/mslibrary/database/impl/internal/MySQLDatabase.java
+++ mslibrary-database/src/main/java/kr/msleague/mslibrary/database/impl/internal/MySQLDatabase.java
@@ -1,6 +1,74 @@
 package kr.msleague.mslibrary.database.impl.internal;
 
-import kr.msleague.mslibrary.database.impl.AbstractDatabase;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import kr.msleague.mslibrary.database.api.DatabaseConfig;
+import kr.msleague.mslibrary.database.api.MSDatabase;
+import kr.msleague.mslibrary.misc.ThrowingFunction;
+import lombok.RequiredArgsConstructor;
 
-public class MySQLDatabase extends AbstractDatabase {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+@RequiredArgsConstructor
+public class MySQLDatabase implements MSDatabase<Connection> {
+
+    final ExecutorService service;
+    HikariDataSource dataSource;
+
+    @Override
+    public boolean connect(DatabaseConfig config) {
+        try {
+            HikariConfig hikariConfig = new HikariConfig();
+            String address = config.getAddress();
+            String port = config.getPort();
+            String database = config.getDatabase();
+            String username = config.getUser();
+            String password = config.getPassword();
+            String autoReconnect = config.get("autoReconnect");
+            String allowMultiQueries = config.get("allowMultiQueries");
+
+            hikariConfig.setJdbcUrl("jdbc:mysql://" + address + ":" + port + "/" + database + "?autoReconnect="+autoReconnect+"&allowMultiQueries="+allowMultiQueries);
+            hikariConfig.setUsername(username);
+            hikariConfig.setPassword(password);
+            hikariConfig.addDataSourceProperty("maximumPoolSize", config.get("maximumPoolSize", "10"));
+            hikariConfig.addDataSourceProperty("cachePrepStmts", config.get("cachePrepStmts","true"));
+            hikariConfig.addDataSourceProperty("prepStmtCacheSize", config.get("prepStmtCacheSize", "250"));
+            hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", config.get("prepStmtCacheSqlLimit", "2048"));
+            hikariConfig.addDataSourceProperty("useServerPrepStmts", config.get("useServerPrepStmts", "true"));
+            hikariConfig.addDataSourceProperty("useLocalSessionState", config.get("useLocalSessionState", "true"));
+            hikariConfig.addDataSourceProperty("rewriteBatchedStatements", config.get("rewriteBatchedStatements", "true"));
+            hikariConfig.addDataSourceProperty("cacheResultSetMetadata", config.get("cacheResultSetMetadata", "true"));
+            hikariConfig.addDataSourceProperty("cacheServerConfiguration", config.get("cacheServerConfiguration", "true"));
+            hikariConfig.addDataSourceProperty("elideSetAutoCommits", config.get("elideSetAutoCommits", "true"));
+            hikariConfig.addDataSourceProperty("maintainTimeStats", config.get("maintainTimeStats", "false"));
+            dataSource = (HikariDataSource) hikariConfig.getDataSource();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public <R> Future<R> executeAsync(ThrowingFunction<Connection, R> function) {
+        try (Connection con = dataSource.getConnection()){
+            return service.submit(() -> function.acceptThrowing(con));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public <R> R execute(ThrowingFunction<Connection, R> function) {
+        try (Connection con = dataSource.getConnection()){
+            return function.acceptThrowing(con);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
