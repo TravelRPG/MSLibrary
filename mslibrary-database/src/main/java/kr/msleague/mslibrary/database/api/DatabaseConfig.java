97fe5f98c0693bd85411c695ee93797e6bd6aeb0
mrminsu <82914006+arkarang@users.noreply.github.com>
2021-08-15 05:11:45 +0900

    document update

--- mslibrary-database/src/main/java/kr/msleague/mslibrary/database/api/DatabaseConfig.java
+++ mslibrary-database/src/main/java/kr/msleague/mslibrary/database/api/DatabaseConfig.java
@@ -1,6 +1,9 @@
 package kr.msleague.mslibrary.database.api;
 
 import com.sun.istack.internal.Nullable;
+import lombok.NonNull;
+
+import java.util.Optional;
 
 /**
  * MS 데이터베이스 설정
@@ -25,7 +28,7 @@ public interface DatabaseConfig {
     }
 
     /**
-     * 데이터베이스 유저를 받아옵니다.
+     * 데이터베이스 유저를 반환합니다.
      * @return 데이터베이스 유저 이름
      */
     @Nullable
@@ -34,40 +37,92 @@ public interface DatabaseConfig {
     }
 
     /**
-     *
-     * @param user
+     * 데이터베이스 유저를 설정합니다.
+     * @param user 데이터베이스 유저 이름
      */
     default void setUser(String user){
         setManually("user", user);
     }
 
-    default int getPort(){
-        return Integer.parseInt(get("port"));
+    /**
+     * 데이터베이스 포트를 반환합니다.
+     * @return 데이터베이스 포트
+     */
+    @Nullable
+    default String getPort(){
+        return get("port");
     }
 
+    /**
+     * 데이터베이스 포트를 설정합니다.
+     * @param port 데이터베이스 포트
+     */
     default void setPort(int port){
         setManually("port", port+"");
     }
 
+    /**
+     * 데이터베이스 비밀번호를 반환합니다.
+     * @return 데이터베이스 비밀번호
+     */
+    @Nullable
     default String getPassword(){
         return get("password");
     }
 
+    /**
+     * 데이터베이스 비밀번호를 설정합니다.
+     * @param password 데이터베이스 비밀번호
+     */
     default void setPassword(String password){
         setManually("password", password);
     }
 
+    /**
+     * 데이터베이스 이름을 반환합니다.
+     * @return 데이터베이스 이름
+     */
+    @Nullable
     default String getDatabase(){
         return get("database");
     }
 
+    /**
+     * 데이터베이스 이름을 설정합니다.
+     * @param database 데이터베이스 이름
+     */
     default void setDatabase(String database){
         setManually("database", database);
     }
 
-    void setManually(String key, String value);
+    /**
+     * 데이터베이스 설정 값을 직접 수정합니다.
+     * @param key 설정 이름
+     * @param value 설정 값
+     */
+    void setManually(@NonNull String key, @Nullable String value);
 
+    /**
+     * 데이터베이스 설정 값을 받아옵니다.
+     * @param key 설정 이름
+     * @return 설정 값
+     */
+    @Nullable
     String get(String key);
 
+    /**
+     * 데이터베이스 설정 값을 받아옵니다.
+     * @param key 설정 이름
+     * @param defaultValue 기본 값
+     * @return 값이 있을 경우 설정 값, 없을 경우 입력받은 기본 값
+     */
+    default String get(@NonNull String key, @NonNull String defaultValue){
+        return Optional.ofNullable(get(key)).orElse(defaultValue);
+    }
+
+    /**
+     * 설정을 복사합니다.
+     * @return 복사 된 데이터베이스 설정
+     */
     DatabaseConfig copy();
 }
