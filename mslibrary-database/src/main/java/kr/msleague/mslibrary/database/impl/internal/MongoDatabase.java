13a8cedc0dcaa5aa9fdfb8b9993c3a8ed10a4605
mrminsu <82914006+arkarang@users.noreply.github.com>
2021-08-15 05:10:03 +0900

    make unimplement

--- mslibrary-database/src/main/java/kr/msleague/mslibrary/database/impl/internal/MongoDatabase.java
+++ mslibrary-database/src/main/java/kr/msleague/mslibrary/database/impl/internal/MongoDatabase.java
@@ -1,6 +1,31 @@
 package kr.msleague.mslibrary.database.impl.internal;
 
-import kr.msleague.mslibrary.database.impl.AbstractDatabase;
+import kr.msleague.mslibrary.database.api.DatabaseConfig;
+import kr.msleague.mslibrary.database.api.MSDatabase;
+import kr.msleague.mslibrary.misc.ThrowingFunction;
+import lombok.RequiredArgsConstructor;
 
-public class MongoDatabase extends AbstractDatabase {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+@RequiredArgsConstructor
+public class MongoDatabase implements MSDatabase<com.mongodb.client.MongoDatabase> {
+
+    final ExecutorService service;
+    com.mongodb.client.MongoDatabase handle;
+
+    @Override
+    public boolean connect(DatabaseConfig config) {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    @Override
+    public <R> Future<R> executeAsync(ThrowingFunction<com.mongodb.client.MongoDatabase, R> function) {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    @Override
+    public <R> R execute(ThrowingFunction<com.mongodb.client.MongoDatabase, R> function) {
+        throw new UnsupportedOperationException("not implemented");
+    }
 }
--- mslibrary-database/src/main/java/kr/msleague/mslibrary/database/impl/internal/RedisDatabase.java
+++ mslibrary-database/src/main/java/kr/msleague/mslibrary/database/impl/internal/RedisDatabase.java
@@ -1,6 +1,37 @@
 package kr.msleague.mslibrary.database.impl.internal;
 
-import kr.msleague.mslibrary.database.impl.AbstractDatabase;
+import io.lettuce.core.api.sync.RedisCommands;
+import kr.msleague.mslibrary.database.api.DatabaseConfig;
+import kr.msleague.mslibrary.database.api.MSDatabase;
+import kr.msleague.mslibrary.misc.ThrowingFunction;
 
-public class RedisDatabase extends AbstractDatabase {
+import java.util.concurrent.Future;
+
+public class RedisDatabase implements MSDatabase<RedisCommands<String, String>> {
+
+    RedisCommands<String, String> syncCommands;
+
+    @Override
+    public boolean connect(DatabaseConfig config) {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    @Override
+    public <R> Future<R> executeAsync(ThrowingFunction<RedisCommands<String, String>, R> function) {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    @Override
+    public <R> R execute(ThrowingFunction<RedisCommands<String, String>, R> function) {
+        throw new UnsupportedOperationException("not implemented");
+        /*
+        try {
+            return function.acceptThrowing(syncCommands);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+         */
+    }
+
 }
