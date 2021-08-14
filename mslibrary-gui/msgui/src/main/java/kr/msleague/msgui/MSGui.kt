d101bfacc5ee70f4ff731a9e178bcb19e09df7df
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2021-08-15 01:25:35 +0900

    hi kotlin

--- mslibrary-gui/msgui/src/main/java/kr/msleague/msgui/MSGui.kt
+++ mslibrary-gui/msgui/src/main/java/kr/msleague/msgui/MSGui.kt
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin
 class MSGui: JavaPlugin() {
 
     override fun onEnable() {
-        println("hi")
+        println("hi kotlin")
     }
 
     override fun onDisable() {
