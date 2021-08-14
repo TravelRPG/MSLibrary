cf2326525289e1936f9f9b5dd56e42e1621e43b0
mrminsu <82914006+arkarang@users.noreply.github.com>
2021-08-15 05:10:31 +0900

    implemented DefaultHikariConfig

--- mslibrary-database/src/main/java/kr/msleague/mslibrary/database/impl/internal/DefaultHikariConfig.java
+++ mslibrary-database/src/main/java/kr/msleague/mslibrary/database/impl/internal/DefaultHikariConfig.java
@@ -1,6 +1,21 @@
 package kr.msleague.mslibrary.database.impl.internal;
 
-import kr.msleague.mslibrary.database.impl.AbstractConfig;
+import com.zaxxer.hikari.HikariConfig;
+import kr.msleague.mslibrary.database.impl.HashDatabaseConfig;
 
-public class DefaultHikariConfig extends AbstractConfig {
+public class DefaultHikariConfig extends HashDatabaseConfig {
+    public DefaultHikariConfig() {
+        this.setManually("maximumPoolSize", "10");
+        this.setManually("autoReconnect", "true");
+        this.setManually("cachePrepStmts", "true");
+        this.setManually("prepStmtCacheSize", "250");
+        this.setManually("prepStmtCacheSqlLimit", "2048");
+        this.setManually("useServerPrepStmts", "true");
+        this.setManually("useLocalSessionState", "true");
+        this.setManually("rewriteBatchedStatements", "true");
+        this.setManually("cacheResultSetMetadata", "true");
+        this.setManually("cacheServerConfiguration", "true");
+        this.setManually("elideSetAutoCommits", "true");
+        this.setManually("maintainTimeStats", "false");
+    }
 }
