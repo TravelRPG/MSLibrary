86abb1fd2bfc3e5c72607ee57cd198166f96cb7a
mrminsu <82914006+arkarang@users.noreply.github.com>
2021-08-15 05:09:40 +0900

    implemented database registery module

--- mslibrary-database/src/main/java/kr/msleague/mslibrary/database/MSLDatabases.java
+++ mslibrary-database/src/main/java/kr/msleague/mslibrary/database/MSLDatabases.java
@@ -1,13 +1,60 @@
 package kr.msleague.mslibrary.database;
 
+import com.mongodb.lang.Nullable;
 import kr.msleague.mslibrary.database.api.DatabaseConfig;
 import kr.msleague.mslibrary.database.api.MSDatabase;
+import lombok.Getter;
+import lombok.NonNull;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * 데이터베이스 모음
+ */
 public class MSLDatabases {
 
-    final ConcurrentHashMap<String, MSDatabase<?>> databases = new ConcurrentHashMap<>();
-    final ConcurrentHashMap<String, DatabaseConfig> defaultConfigs = new ConcurrentHashMap<>();
+    @Getter
+    private static final MSLDatabases inst = new MSLDatabases();
+
+    private final ConcurrentHashMap<String, MSDatabase<?>> databases = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, DatabaseConfig> defaultConfigs = new ConcurrentHashMap<>();
+
+    private MSLDatabases(){}
+
+    public void add(@NonNull String name, @NonNull MSDatabase<?> database) throws IllegalArgumentException{
+        if(databases.containsKey("name"))
+            throw new IllegalArgumentException("the name "+name+" is already exist");
+        else
+            databases.put(name, database);
+    }
+
+    public MSDatabase<?> remove(@NonNull String name){
+        if(databases.containsKey("name"))
+            return databases.remove(name);
+        else
+            throw new IllegalArgumentException("the name "+name+" is not exist");
+    }
+
+    @Nullable
+    public MSDatabase<?> get(String name){
+        return databases.get(name);
+    }
+
+    @Nullable
+    public DatabaseConfig getDefaultConfig(@NonNull String name){
+        return defaultConfigs.get(name);
+    }
+
+    @Nullable
+    public void addDefaultConfig(@NonNull String name, @NonNull DatabaseConfig defaultConfig){
+        if(defaultConfigs.containsKey(name))
+            throw new IllegalArgumentException("the name "+name+" is already exist");
+        else
+            defaultConfigs.put(name, defaultConfig);
+    }
+
+    public void removeDefaultConfig(@NonNull String name){
+        defaultConfigs.remove(name);
+    }
 
 }
