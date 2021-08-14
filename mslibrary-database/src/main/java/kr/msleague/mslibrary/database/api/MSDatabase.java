c3771af68ba8c734348783b82865d4eed043ae58
mrminsu <82914006+arkarang@users.noreply.github.com>
2021-08-15 02:09:10 +0900

    Update MSDatabase.java

--- mslibrary-database/src/main/java/kr/msleague/mslibrary/database/api/MSDatabase.java
+++ mslibrary-database/src/main/java/kr/msleague/mslibrary/database/api/MSDatabase.java
@@ -1,4 +1,36 @@
 package kr.msleague.mslibrary.database.api;
 
-public interface MSDatabase {
+import kr.msleague.mslibrary.misc.ThrowingFunction;
+
+import java.io.Closeable;
+import java.util.concurrent.Future;
+
+/**
+ * MS 데이터베이스 인터페이스
+ * @param <T> 데이터베이스 커넥션 타입 ( Connection , MongoCollection, RedisCommands 같은거 ㅇㅇ )
+ */
+public interface MSDatabase<T> {
+
+    /**
+     * 데이터베이스를 연결합니다.
+     * @param config 주어진 데이터베이스 설정
+     * @return 연결 성공 여부
+     */
+    boolean connect(DatabaseConfig config);
+
+    /**
+     * 로직을 비동기로 실행합니다.
+     * @param function 실행할 로직
+     * @param <R> 반환 값의 타입
+     * @return 실행 완료 시 반환 값
+     */
+    <R> Future<R> executeAsync(ThrowingFunction<T, R> function);
+
+    /**
+     * 로직을 동기로 실행합니다.
+     * @param function 실행할 로직
+     * @param <R> 반환 값의 타입
+     * @return 실행 완료 시 반환 값
+     */
+    <R> R execute(ThrowingFunction<T, R> function);
 }
