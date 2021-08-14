7d0c29bfb66bed7ef675a684833b0040d73dd5df
mrminsu <82914006+arkarang@users.noreply.github.com>
2021-08-15 02:08:16 +0900

    Create ThrowingFunction.java

---
+++ mslibrary-misc/src/main/java/kr/msleague/mslibrary/misc/ThrowingFunction.java
@@ -0,0 +1,18 @@
+package kr.msleague.mslibrary.misc;
+
+import java.util.function.Function;
+
+public interface ThrowingFunction<T, R> extends Function<T, R> {
+
+    @Override
+    default R apply(T t) {
+        try{
+            return acceptThrowing(t);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    R acceptThrowing(T t) throws Exception;
+}
