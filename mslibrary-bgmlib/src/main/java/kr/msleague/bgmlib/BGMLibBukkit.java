1d28f22ac0d9c2ca6717cb27de2ea7e5ea0eb263
ChuYong <jakgon@naver.com>
2021-08-15 16:23:32 +0900

    Updated BGMLib & SoaLib

---
+++ mslibrary-bgmlib/src/main/java/kr/msleague/bgmlib/BGMLibBukkit.java
@@ -0,0 +1,14 @@
+package kr.msleague.bgmlib;
+
+import kr.msleague.bootstrap.MSPlugin;
+
+public class BGMLibBukkit extends MSPlugin {
+    @Override
+    public void onEnable(){
+        //Plugin start logic
+    }
+    @Override
+    public void onDisable(){
+
+    }
+}
---
+++ mslibrary-soalib/src/main/java/kr/msleague/soalib/SOALibBukkit.java
@@ -0,0 +1,14 @@
+package kr.msleague.soalib;
+
+import kr.msleague.bootstrap.MSPlugin;
+
+public class SOALibBukkit extends MSPlugin {
+    @Override
+    public void onEnable(){
+        //Plugin start logic
+    }
+    @Override
+    public void onDisable(){
+
+    }
+}
