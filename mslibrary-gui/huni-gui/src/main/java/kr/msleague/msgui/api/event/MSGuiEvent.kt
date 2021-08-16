cd4392ee73fe3bf909b1b91f7cedf4baca6409b0
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2021-08-16 23:08:03 +0900

    Update MSGuiEvent.kt

--- mslibrary-gui/huni-gui/src/main/java/kr/msleague/msgui/api/event/MSGuiEvent.kt
+++ mslibrary-gui/huni-gui/src/main/java/kr/msleague/msgui/api/event/MSGuiEvent.kt
@@ -8,12 +8,4 @@ import org.bukkit.event.HandlerList
 abstract class MSGuiEvent(
     val player: Player,
     val gui: MSGui<*>
-): Event() {
-
-    override fun getHandlers(): HandlerList = HANDLER_LIST
-    companion object {
-        var HANDLER_LIST: HandlerList = HandlerList()
-        @JvmStatic fun getHandlerList(): HandlerList = HANDLER_LIST
-    }
-
-}
+): Event()
