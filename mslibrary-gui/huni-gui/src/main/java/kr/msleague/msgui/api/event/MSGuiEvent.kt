a58179bda320b65db55fa33a28af38286bc1fdc0
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2021-08-16 23:16:30 +0900

    remove TestGui.java

--- mslibrary-gui/huni-gui/src/main/java/kr/msleague/msgui/api/event/MSGuiEvent.kt
+++ mslibrary-gui/huni-gui/src/main/java/kr/msleague/msgui/api/event/MSGuiEvent.kt
@@ -3,7 +3,6 @@ package kr.msleague.msgui.api.event
 import kr.msleague.msgui.gui.MSGui
 import org.bukkit.entity.Player
 import org.bukkit.event.Event
-import org.bukkit.event.HandlerList
 
 abstract class MSGuiEvent(
     val player: Player,
