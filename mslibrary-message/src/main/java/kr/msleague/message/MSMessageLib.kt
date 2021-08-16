8d788824f93572989552a7be08abffbafade8821
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2021-08-17 02:53:50 +0900

    Init Module - MSMessage Library

---
+++ mslibrary-message/src/main/java/kr/msleague/message/MSMessageLib.kt
@@ -0,0 +1,9 @@
+package kr.msleague.message
+
+import kr.msleague.bootstrap.MSPlugin
+
+class MSMessageLib: MSPlugin() {
+
+    override fun onEnable() { plugin = getPlugin() }
+
+}
---
+++ mslibrary-message/src/main/java/kr/msleague/message/Variables.kt
@@ -0,0 +1,9 @@
+package kr.msleague.message
+
+import org.bukkit.Server
+import org.bukkit.plugin.PluginManager
+import org.bukkit.plugin.java.JavaPlugin
+
+lateinit var plugin: JavaPlugin
+val server: Server by lazy { plugin.server }
+val pluginManager: PluginManager by lazy { plugin.server.pluginManager }
