af8ae1aeab275906c20c30bfce2d9766a8bb8ca4
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2021-08-17 13:54:08 +0900

    MSLeague Message Variables.kt is Kut!!!!!!!

--- mslibrary-message/src/main/java/kr/msleague/message/Variables.kt
+++
@@ -1,9 +0,0 @@
-package kr.msleague.message
-
-import org.bukkit.Server
-import org.bukkit.plugin.PluginManager
-import org.bukkit.plugin.java.JavaPlugin
-
-lateinit var plugin: JavaPlugin
-val server: Server by lazy { plugin.server }
-val pluginManager: PluginManager by lazy { plugin.server.pluginManager }
