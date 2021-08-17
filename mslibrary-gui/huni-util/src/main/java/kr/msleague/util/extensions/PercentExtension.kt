31c7803003bbe3db7ac15fe2e3fca7b97954e0b5
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2021-08-18 03:12:09 +0900

    Add Percent Utility for Kotlin

---
+++ mslibrary-gui/huni-util/src/main/java/kr/msleague/util/extensions/PercentExtension.kt
@@ -0,0 +1,36 @@
+package kr.msleague.util.extensions
+
+import java.lang.IllegalArgumentException
+import java.math.BigDecimal
+import java.util.AbstractMap
+import kotlin.math.ln
+import kotlin.random.Random
+
+fun Double.percent(): Boolean {
+    val temp = this
+    if(temp >= 100.0) return true
+    else if(temp <= 0.0) return false
+    val random = Random
+    val suc = BigDecimal(this)
+    var fail = BigDecimal("100.0")
+    fail = fail.subtract(suc)
+    val entry = mapOf(true to suc.toDouble(),false to fail.toDouble()).entries.stream()
+    return entry
+        .map { e-> AbstractMap.SimpleEntry(e.key, -ln(random.nextDouble()) /e.value) }
+        .min(compareBy { it.value })
+        .orElseThrow { IllegalArgumentException() }.key
+}
+
+fun Int.percent(): Boolean {
+    val temp = toDouble()
+    return temp.percent()
+}
+
+inline fun <reified T> Map<T,Double>.percent(): T {
+    val random = Random
+    val entry = entries.stream()
+    return entry
+        .map { e-> AbstractMap.SimpleEntry(e.key, -ln(random.nextDouble()) /e.value) }
+        .min(compareBy { it.value })
+        .orElseThrow { IllegalArgumentException() }.key
+}
