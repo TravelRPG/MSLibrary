67a4d4a71a5691fcf32a216668d640bd4db872a0
ChuYong <jakgon@naver.com>
2021-08-19 22:30:05 +0900

    Update PingPongManagementThread.java

--- mslibrary-communication/bcsp-proxy/src/main/java/kr/msleague/bcsp/proxy/PingPongManagementThread.java
+++ mslibrary-communication/bcsp-proxy/src/main/java/kr/msleague/bcsp/proxy/PingPongManagementThread.java
@@ -3,6 +3,7 @@ package kr.msleague.bcsp.proxy;
 import kr.msleague.bcsp.internal.netty.channel.ChannelWrapper;
 import kr.msleague.bcsp.internal.netty.packet.AbstractPacket;
 import kr.msleague.bcsp.internal.netty.packet.sys.PingPongPacket;
+import kr.msleague.bcsp.internal.netty.pipeline.ConnectionState;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,6 +15,13 @@ public class PingPongManagementThread extends Thread{
     @Override
     public void run(){
         while(wrapper.getChannel().isOpen() && wrapper.getChannel().isActive()){
+            try{
+                TimeUnit.SECONDS.sleep(1);
+            }catch(InterruptedException ex){
+                ex.printStackTrace();
+            }
+            if(wrapper.getHandler().getConnectionState() != ConnectionState.CONNECTED)
+                return;
             wrapper.startCallBack(new PingPongPacket(-1L, System.currentTimeMillis()), PingPongPacket.class, packet->{
                 long ping = System.currentTimeMillis() - packet.getRecievedTime();
                 wrapper.getPingCalculator().processPing(ping);
@@ -21,11 +29,6 @@ public class PingPongManagementThread extends Thread{
                 rt.setCallBackResult(true);
                 wrapper.sendPacket(rt);
             });
-            try{
-                TimeUnit.SECONDS.sleep(1);
-            }catch(InterruptedException ex){
-                ex.printStackTrace();
-            }
         }
 
     }
