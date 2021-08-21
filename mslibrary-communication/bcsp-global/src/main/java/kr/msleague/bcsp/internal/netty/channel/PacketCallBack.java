dd9fa80b243717e7ec5fd0f9959ced3b7c34c0c6
ChuYong <jakgon@naver.com>
2021-08-21 22:38:46 +0900

    Update annotation

--- mslibrary-communication/bcsp-global/src/main/java/kr/msleague/bcsp/internal/netty/channel/PacketCallBack.java
+++ mslibrary-communication/bcsp-global/src/main/java/kr/msleague/bcsp/internal/netty/channel/PacketCallBack.java
@@ -3,5 +3,9 @@ package kr.msleague.bcsp.internal.netty.channel;
 import kr.msleague.bcsp.internal.netty.packet.AbstractPacket;
 
 public interface PacketCallBack<T extends AbstractPacket> {
+    /**
+     * The interface used to packet callback
+     * @param t Target packet
+     */
     void onCallBackRecieved(T t);
 }
