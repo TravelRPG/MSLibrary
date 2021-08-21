f9b2200715de2475da43ba65ad9dded42e15c0de
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2021-08-22 01:26:13 +0900

    MSMessageAdapter#request result is Nullable

--- mslibrary-message/src/main/java/kr/msleague/message/api/MSMessageAdapter.kt
+++ mslibrary-message/src/main/java/kr/msleague/message/api/MSMessageAdapter.kt
@@ -1,7 +1,5 @@
 package kr.msleague.message.api
 
-import javax.annotation.Nullable
-
 interface MSMessageAdapter<T> {
-    @Nullable fun request(obj: T, placeHolder: String): String?
+    fun request(obj: T, placeHolder: String): String?
 }
