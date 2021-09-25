784a77ba189174c3b51a4805271f5f3aa0521998
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2021-09-26 02:59:56 +0900

    Update SkullManager.kt

--- mslibrary-gui/huni-gui/src/main/java/kr/msleague/msgui/managers/SkullManager.kt
+++ mslibrary-gui/huni-gui/src/main/java/kr/msleague/msgui/managers/SkullManager.kt
@@ -10,20 +10,27 @@ import java.lang.reflect.Field
 import java.util.*
 
 object SkullManager {
+
+    private val base64Method = Class.forName("org.bukkit.craftbukkit.libs.org.apache.commons.codec.binary.Base64").getMethod("encodeBase64",ByteArray::class.java)
+    private fun base64Func(array: ByteArray): ByteArray { return base64Method.invoke(null,array) as ByteArray }
+
     fun getSkull(temp: String, amount: Int): ItemStack {
         val head = if(highVersion) ItemStack(Material.valueOf("PLAYER_HEAD"), amount) else ItemStack(Material.SKULL_ITEM, amount, 3.toShort())
         if (temp.isEmpty()) return head
         val url = "https://textures.minecraft.net/texture/$temp"
-        val headMeta = head.itemMeta
+        val headMeta = head.itemMeta!!
         val profile = GameProfile(UUID.randomUUID(), null)
-        val encodedData = Base64.encodeBase64(String.format("{textures:{SKIN:{url:\"%s\"}}}", url).toByteArray())
+        val array = String.format("{textures:{SKIN:{url:\"%s\"}}}", url).toByteArray()
+        val encodedData = if(highVersion) base64Func(array) else Base64.encodeBase64(array)
         profile.properties.put("textures", Property("textures", String(encodedData)))
         var profileField: Field? = null
         try {
-            profileField = headMeta.javaClass.getDeclaredField("profile")
+            profileField = headMeta::class.java.getDeclaredField("profile")
         } catch (e: NoSuchFieldException) {
             e.printStackTrace()
         } catch (e: SecurityException) {
+            e.printStackTrace()
+        } catch (e: NullPointerException) {
             e.printStackTrace()
         }
         profileField?.isAccessible = true
