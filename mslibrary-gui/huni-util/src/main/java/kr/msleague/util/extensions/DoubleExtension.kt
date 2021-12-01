494ee6e52bde11472969d5ce109f3b52d70776af
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2021-12-01 09:19:44 +0900

    Update DoubleExtension.kt

--- mslibrary-gui/huni-util/src/main/java/kr/msleague/util/extensions/DoubleExtension.kt
+++ mslibrary-gui/huni-util/src/main/java/kr/msleague/util/extensions/DoubleExtension.kt
@@ -1,8 +1,8 @@
 package kr.msleague.util.extensions
 
-import kotlin.math.pow
+import kotlin.math.roundToInt
 
-fun Double.round(n: Int = 3): Double = this * 10.0.pow(n) / 10.0.pow(n)
+fun Double.round(n: Int = 3): Double = (this * (10 * n)).roundToInt() / (10 * n).toDouble()
 fun Double.roundString(n: Int = 3): String = round(n).toString()
 fun Float.round(n: Int = 3): Float = this.toDouble().round(n).toFloat()
 fun Float.roundString(n: Int = 3): String = this.toDouble().roundString(n)
