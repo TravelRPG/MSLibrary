dd8d39f6d646d10a8a9c8c41839a48ce80606a40
ChuYong <67673493+CChuYong@users.noreply.github.com>
2022-02-25 19:38:23 +0900

    1.17.1 update

--- mslibrary-inventory/src/main/java/kr/msleague/inventory/nms/V1_17_1.java
+++ mslibrary-inventory/src/main/java/kr/msleague/inventory/nms/V1_17_1.java
@@ -1,16 +1,53 @@
 package kr.msleague.inventory.nms;
 
+import com.google.common.base.Preconditions;
+import kr.msleague.inventory.CompressAPI;
 import kr.msleague.inventory.ItemStackSerializerAdapter;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+
+import java.io.*;
 
 public class V1_17_1 implements ItemStackSerializerAdapter {
     @Override
     public byte[] serialize(ItemStack itemStack, boolean compress) {
-        return new byte[0];
+        Preconditions.checkNotNull(itemStack, "Input item cannot be null");
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream(); DataOutputStream dos = new DataOutputStream(baos)) {
+            writeItemStackData(itemStack, dos);
+            dos.flush();
+            byte[] target = baos.toByteArray();
+            return CompressAPI.compress(target, compress);
+        } catch (IOException ex) {
+            return null;
+        }
     }
 
     @Override
     public ItemStack deserialize(byte[] array) {
-        return null;
+        array = CompressAPI.decompress(array);
+        try (ByteArrayInputStream bais = new ByteArrayInputStream(array); DataInputStream dis = new DataInputStream(bais); BukkitObjectInputStream bis = new BukkitObjectInputStream(dis)) {
+//            net.minecraft.world.item.ItemStack is = new net.minecraft.world.item.ItemStack (CraftMagicNumbers.getItem(Material.getMaterial(dis.readUTF())), dis.readInt());
+//            if (dis.readBoolean()) {
+//                is.setTag(NBTCompressedStreamTools.a((DataInput) dis));
+//            }
+            return (ItemStack) bis.readObject();
+        } catch (IOException | ClassNotFoundException ex) {
+            return null;
+        }
+    }
+
+    private void writeItemStackData(ItemStack itemStack, DataOutputStream dos) throws IOException {
+//        dos.writeUTF(itemStack.getType().name());
+//        dos.writeInt(itemStack.getAmount());
+//        dos.writeShort(itemStack.getDurability());
+//        NBTTagCompound tc = CraftItemStack.asNMSCopy(itemStack).getTag();
+//        dos.writeBoolean(tc != null);
+//        if (tc != null) {
+//            NBTCompressedStreamTools.a(tc, (DataOutput) dos);
+//        }
+        BukkitObjectOutputStream output = new BukkitObjectOutputStream(dos);
+        output.writeObject(itemStack);
+        output.close();
     }
 }
--- mslibrary-misc/src/main/java/kr/msleague/mslibrary/misc/itemcomp/ItemStackComparatorFunction.java
+++ mslibrary-misc/src/main/java/kr/msleague/mslibrary/misc/itemcomp/ItemStackComparatorFunction.java
@@ -1,8 +1,7 @@
 package kr.msleague.mslibrary.misc.itemcomp;
 
 import kr.msleague.mslibrary.misc.Pair;
-import org.bukkit.inventory.ItemStack;
 import java.util.function.Predicate;
 
-public interface ItemStackComparatorFunction extends Predicate<Pair<ItemStack, ItemStack>> {
+public interface ItemStackComparatorFunction  {
 }
